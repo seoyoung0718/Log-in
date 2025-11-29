11fbc03d50a5792905f64e7ab53e1fdd40c4b980
ksy132336 <ksygiru@gmail.com>
2025-11-29 23:37:08 +0900

    feat: MySQL DB 연결 및 Sequelize 설정 코드 작성

---
+++ models/index.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const Sequelize = require("sequelize");
+const path = require("path");
+const fs = require("fs");
+
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config.js")[env];
+const db = {};
+
+let sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 &&
+      file !== path.basename(__filename) &&
+      file.endsWith(".js")
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
