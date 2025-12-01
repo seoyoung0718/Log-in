d1cb49dadae4b84e4d6e6cf1a3fddad6daf6ad33
ksy132336 <ksygiru@gmail.com>
2025-12-01 19:13:21 +0900

    feat: 회원가입 및 로그인 처리 라우터 구현

---
+++ src/routes/auth.js
@@ -0,0 +1,22 @@
+// 회원가입 처리 라우터
+router.post("/signup", async (req, res) => {
+  const { email, password, nickname } = req.body;
+  const hashed = await bcrypt.hash(password, 10);
+
+  await User.create({
+    email,
+    password: hashed,
+    nickname,
+  });
+
+  res.redirect("/auth/login");
+});
+
+// 로그인 처리 라우터
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login",
+  })
+);
