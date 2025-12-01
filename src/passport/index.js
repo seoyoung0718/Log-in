7294f91a766f0f8c9be2c912b09bcddb0003a88c
ksy132336 <ksygiru@gmail.com>
2025-12-01 18:55:14 +0900

    feat: Passport 직렬화/역직렬화 로직 구현

---
+++ src/passport/index.js
@@ -0,0 +1,19 @@
+const local = require("./localStrategy");
+const { User } = require("../models");
+
+module.exports = (passport) => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findByPk(id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+
+  local(passport);
+};
